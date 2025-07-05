c75b6e65515cc770d24a09b5f995908df60f5300
Sindhu5653 <sindhupvly@gmail.com>
2025-07-05 15:05:40 +0530

    best sellers

--- GREENCART/Client/src/components/BestSeller.jsx
+++ GREENCART/Client/src/components/BestSeller.jsx
@@ -3,12 +3,14 @@ import ProductCard from './ProductCard'
 import { useAppContext } from '../context/AppContext'
 
 const BestSeller = () => {
-    const { products } = useAppContext
+    const { products } = useAppContext(); 
     return (
         <div className='mt-16'>
             <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
-            <div>
-                <ProductCard product={products[0]} />
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
+                {products.filter((product)=>product.inStock).slice(0,5).map((product, index)=>(
+                    <ProductCard key={index} product={product} />
+                ))}
             </div>
         </div>
     )
