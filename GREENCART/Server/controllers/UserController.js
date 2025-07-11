a3b19e332b490e7fdd27129a26a18898a9dc00c7
Sindhu5653 <sindhupvly@gmail.com>
2025-07-11 15:37:36 +0530

    backend

--- GREENCART/Server/controllers/UserController.js
+++ GREENCART/Server/controllers/UserController.js
@@ -62,12 +62,42 @@ export const login = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            maxAge: 7 * 24 * 60 * 1000,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
         return res.json({ success: true, user: { email: user.email, name: user.name } })
 
 
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//Check Auth : /api/user/is-auth
+
+export const isAuth = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const user = await User.findById(userId).select("-password")
+        return res.json({ success: true, user })
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//Login User : /api/user/logout
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+        return res.json({ success: true, message: 'Logged Out' })
     } catch (error) {
         console.log(error.message)
         res.json({ success: false, message: error.message })
