d9f62138a0dcaea8abbd4e590fa5ffd1260f48ad
Sindhu5653 <sindhupvly@gmail.com>
2025-07-13 00:08:47 +0530

    backend

--- GREENCART/Server/server.js
+++ GREENCART/Server/server.js
@@ -5,12 +5,16 @@ import connectDB from './configs/db.js';
 import 'dotenv/config'; 
 import userRouter from './routes/UserRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 await connectDB()
+await connectCloudinary()
 
 //Allow multiple origins
 const allowedOrigins = ['http://localhost:5173']
@@ -24,6 +28,8 @@ app.get('/', (req, res) => res.send("API is Working"));
 
 app.use('/api/user', userRouter)
 app.use('/api/seller', sellerRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
