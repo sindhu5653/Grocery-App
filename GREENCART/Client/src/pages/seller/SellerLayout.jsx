1f235c7e965b970ec3c6b6e2e0f07d462a46cbb5
Sindhu5653 <sindhupvly@gmail.com>
2025-07-16 10:40:16 +0530

    logout

--- GREENCART/Client/src/pages/seller/SellerLayout.jsx
+++ GREENCART/Client/src/pages/seller/SellerLayout.jsx
@@ -14,18 +14,18 @@ const SellerLayout = () => {
     ];
 
     const logout = async () => {
-       try {
-        const {data} = await axios.get('/api/seller/logout')
-        if(data.success){
-            toast.success(data.message)
-            navigate('/')
-        }
-        else{
-            toast.error(data.message)
-        }
-       } catch (error) {
+        try {
+            const { data } = await axios.get('/api/seller/logout')
+            if (data.success) {
+                toast.success(data.message)
+                navigate('/')
+            }
+            else {
+                toast.error(data.message)
+            }
+        } catch (error) {
             toast.error(error.message)
-       }
+        }
     }
 
     return (
@@ -42,21 +42,21 @@ const SellerLayout = () => {
                 </div>
             </div>
             <div className="flex">
-            <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
-                {sidebarLinks.map((item) => (
-                    <NavLink to={item.path} key={item.name} end={item.path==="/seller"}
-                        className={({isActive})=>`flex items-center py-3 px-4 gap-3 
+                <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col">
+                    {sidebarLinks.map((item) => (
+                        <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
+                            className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
                             ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-indigo-500"
-                                : "hover:bg-gray-100/90 border-white"
-                            }`
-                        }
-                    >
-                        <img src={item.icon} alt="" className="w-7 h-7"/>
-                        <p className="md:block hidden text-center">{item.name}</p>
-                    </NavLink>
-                ))}
-            </div>
-            <Outlet/>
+                                    : "hover:bg-gray-100/90 border-white"
+                                }`
+                            }
+                        >
+                            <img src={item.icon} alt="" className="w-7 h-7" />
+                            <p className="md:block hidden text-center">{item.name}</p>
+                        </NavLink>
+                    ))}
+                </div>
+                <Outlet />
             </div>
         </>
     );
